0737c4081840a3ed34e1b79a83ab7845ff50ac62
erdemoden <erdemoden5@gmail.com>
2021-02-15 17:01:04 +0300

    Initial Commit

---
+++ deneme2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deneme2
+//
+//  Created by erdem öden on 15.02.2021.
+//  Copyright © 2021 erdem öden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
