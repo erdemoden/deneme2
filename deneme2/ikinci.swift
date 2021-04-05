1922814d2505b0946b66133be3dc44e7f3b415f7
erdemoden <erdemoden5@gmail.com>
2021-04-05 22:15:24 +0300

    first commit

---
+++ deneme2/ikinci.swift
@@ -0,0 +1,30 @@
+//
+//  ikinci.swift
+//  deneme2
+//
+//  Created by erdem öden on 15.02.2021.
+//  Copyright © 2021 erdem öden. All rights reserved.
+//
+
+import UIKit
+
+class ikinci: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
